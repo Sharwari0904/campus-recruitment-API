de9ef7ec5cf0f498643c3cf63b376954c1feffda
Sharwari0904 <166250693+Sharwari0904@users.noreply.github.com>
2026-01-31 01:42:37 +0530

    Add files via upload

---
+++ backend/controller/JobPostingController.java
@@ -0,0 +1,46 @@
+package com.tka.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tka.entity.JobPosting;
+import com.tka.service.JobPostingServiceImpl;
+
+@RestController
+@RequestMapping("/job-postings")
+@CrossOrigin("*")
+public class JobPostingController {
+
+	@Autowired
+	private JobPostingServiceImpl jobPostingServiceImpl;
+
+	@PostMapping("/createJob")
+	public JobPosting createJob(@RequestBody JobPosting jobPosting) {
+		return jobPostingServiceImpl.createJobPosting(jobPosting);
+	}
+
+	@GetMapping("/allJobs")
+	public List<JobPosting> getAllJobs() {
+		return jobPostingServiceImpl.getAllJobPostings();
+	}
+
+	@DeleteMapping("/deleteJob/{id}")
+	public String deleteJob(@PathVariable Long id) {
+		return jobPostingServiceImpl.deleteJobPosting(id);
+	}
+
+	@PutMapping("/updateJob")
+	public JobPosting updateJob(@RequestBody JobPosting jobPosting) {
+		return jobPostingServiceImpl.updateJobPosting(jobPosting);
+	}
+}
